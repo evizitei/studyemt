9966d6671e647026ef65b86c8670dbbaeffaa6b6
Ethan Vizitei <ethanvizitei@ethan-viziteis-macbook-pro.local>
2010-02-21 21:44:41 -0600

    some refactoring to dedupe respond blocks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,33 +3,21 @@ class UsersController < ApplicationController
   # GET /users.xml
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
+    respond_block(@users)
   end
 
   # GET /users/1
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
+    respond_block(@user)
   end
 
   # GET /users/new
   # GET /users/new.xml
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user }
-    end
+    respond_block(@user)
   end
 
   # GET /users/1/edit
@@ -80,6 +68,14 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html { redirect_to(users_url) }
       format.xml  { head :ok }
+    end
+  end
+  
+protected
+  def respond_block(xml)
+    respond_to do |format|
+      format.html 
+      format.xml  { render :xml => xml }
     end
   end
 end
